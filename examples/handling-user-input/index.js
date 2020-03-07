2a3f56a9d1f31da27c338352333507a533b2452c
Yury <yuri.inpost@yandex.ru>
2020-03-07 12:33:21 +0300

    complete(theme): работа с пользовательским вводом
    изучил дерективы v-on и v-model
    
    МЕТА ДАННЫЕ: https://ru.vuejs.org/v2/guide/index.html#%D0%A0%D0%B0%D0%B1%D0%BE%D1%82%D0%B0-%D1%81-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D0%BC-%D0%B2%D0%B2%D0%BE%D0%B4%D0%BE%D0%BC

---
+++ examples/handling-user-input/index.js
@@ -0,0 +1,21 @@
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Привет, Vue.js!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split('')
+        .reverse()
+        .join('');
+    }
+  }
+});
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Привет, Vue!'
+  }
+});
